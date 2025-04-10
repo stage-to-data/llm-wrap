779904b4f813f1d4be99a830bbb5e093d0d5af90
Jacob <Hart>
2025-04-10 15:44:05 +0200

    integrate claude

--- llmwrap/__init__.py
+++ llmwrap/__init__.py
@@ -2,3 +2,4 @@ from .prompt import Prompt
 from .ollama_wrapper import OllamaWrapper
 from .openai_wrapper import OpenAIWrapper
 from .corpus import get_image
+from .claude_wrapper import ClaudeWrapper
